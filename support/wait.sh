d034cb032a00657d325c237c89ffe302c58dbca5
ericminio <eric.mignot@gmail.com>
2021-01-08 15:51:56 -0800

    better wait

--- support/wait.sh
+++ support/wait.sh
@@ -2,7 +2,7 @@
 
 while ! /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Forever21! -Q "SELECT 1";
 do
-    echo waiting;
+    echo "waiting for SqlServer";
     sleep 1;
 done;
-echo ready;
+echo "SqlServer is ready";
