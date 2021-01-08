cecd1eaf4659761a235d3684b136c561ea68ac28
ericminio <eric.mignot@gmail.com>
2021-01-08 09:21:20 -0800

    execute can take query or file

--- sqlserver/demo/lead/insert-products.sql
+++ sqlserver/demo/lead/insert-products.sql
@@ -7,8 +7,6 @@ delete PRODUCT
 GO
 
 insert into PRODUCT (CREATION, FIELD) values ('2015-01-01', 'Love')
-GO
 insert into PRODUCT (CREATION, FIELD) values ('2015-01-02', 'Happiness')
-GO
 insert into PRODUCT (CREATION, FIELD) values ('2015-01-03', 'Beauty')
 GO
--- sqlserver/support/execute.sh
+++ sqlserver/support/execute.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 function execute {
-    /opt/mssql-tools/bin/sqlcmd -m 1 -S localhost -U SA -P Forever21! -d exploration -i $1
+    if test -f "$1"; then
+        /opt/mssql-tools/bin/sqlcmd -m 1 -S localhost -U SA -P Forever21! -d exploration -i $1
+    else
+        /opt/mssql-tools/bin/sqlcmd -m 1 -S localhost -U SA -P Forever21! -d exploration -Q "$1"
+    fi
+    
 }
