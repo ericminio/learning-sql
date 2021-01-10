9695adf068499f0edb0a3fb94ac9ab4608b78991
ericminio <eric.mignot@gmail.com>
2021-01-09 19:38:40 -0800

    better wait

--- oracle/support/init.sh
+++ oracle/support/init.sh
@@ -2,9 +2,12 @@
 
 export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 
-while ! echo " select 'yes' as ORACLE_IS_READY from dual;" | /u01/app/oracle/product/11.2.0/xe/bin/sqlplus -s SYSTEM/oracle@localhost
+ready=0
+while [ "$ready" != "1" ]
 do
     echo "waiting for Oracle";
+    echo " select 'yes' as ORACLE_IS_READY from dual;" | /u01/app/oracle/product/11.2.0/xe/bin/sqlplus -s SYSTEM/oracle@localhost > ./oracle/support/init.output
+    ready=`grep yes ./oracle/support/init.output | wc -l`
     sleep 1;
 done;
 echo "Oracle is ready";
