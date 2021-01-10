fce6b5f769af455be3669ae0afc5baa0e32f6983
ericminio <eric.mignot@gmail.com>
2021-01-09 18:39:59 -0800

    Travis, do you see a sqlserver test failure?

--- sqlserver/lead-lag/detect-pick.sh
+++ sqlserver/lead-lag/detect-pick.sh
@@ -10,5 +10,5 @@ function test_lead_and_lag_can_help_detect_picks_in_dataset {
     execute ${DIR}/detect-pick.sql > ${DIR}/run.output
     actual=`cat ${DIR}/run.output | head -3 | tail -1 | cut -d' ' -f 1`
 
-    assertequals "$actual" "Joy"
+    assertequals "$actual" "Joyy"
 }
