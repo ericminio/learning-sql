bed6f1cdb4e48c761aacb876b2b68fd328d5bc82
ericminio <eric.mignot@gmail.com>
2021-01-13 15:03:34 -0800

    psql messages set to warning

--- postgresql/init.sh
+++ postgresql/init.sh
@@ -5,14 +5,19 @@ source ./support/dir.sh
 function executeFile {
     psql exploration -U dev -q -f $1
 }
+function execute {
+    psql exploration -U dev -q -c "$1"
+}
 
 DIR=$(current_dir ${BASH_SOURCE[0]})
 ready=0
 while [ "$ready" != "1" ]
 do
     echo "waiting for database";
-    psql exploration -U dev -c "select 'yes' DATABASE_IS_READY" > $DIR/init.output
+    execute "select 'yes' DATABASE_IS_READY" > $DIR/init.output
     ready=`grep yes $DIR/init.output | wc -l`
     sleep 1;
 done;
 echo "database is ready";
+
+execute "alter database exploration set client_min_messages to WARNING"
