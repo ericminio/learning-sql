eecb9bdc502b51596e9d80fbfda759ce4d59d521
ericminio <eric.mignot@gmail.com>
2021-02-17 20:00:04 -0800

    show me

--- sqlserver/json/json-value.sh
+++ sqlserver/json/json-value.sh
@@ -4,6 +4,7 @@ function test_can_read_value_in_json {
     DIR=$(current_dir ${BASH_SOURCE[0]})
     
     executeFile ${DIR}/json-value.sql > ${DIR}/run.output
+    cat ${DIR}/run.output
     actual=`cat ${DIR}/run.output | head -n 3 | tail -n 1 | cut -d' ' -f1`
 
     assertequals "$actual" "15"
