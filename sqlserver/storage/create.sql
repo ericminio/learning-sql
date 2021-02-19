84c4e26b549b9fdad217023df088c2e62c9fce1a
ericminio <eric.mignot@gmail.com>
2021-02-18 20:41:19 -0800

    fixed

--- sqlserver/storage/create.sql
+++ sqlserver/storage/create.sql
@@ -6,14 +6,14 @@ GO
 DROP TABLE IF EXISTS PRODUCT
 GO
 CREATE TABLE PRODUCT (
-    CREATION_DATE datetime default (getdate()),
+    CREATION_DATE date,
     Field8kbLong varchar(8000)
 )
 GO
 
-insert into PRODUCT (Field8kbLong) values (replicate('A', 4000))
-insert into PRODUCT (Field8kbLong) values (replicate('B', 8000))
-insert into PRODUCT (Field8kbLong) values (replicate('C', 4000))
+insert into PRODUCT (CREATION_DATE, Field8kbLong) values ('2021-01-01', replicate('A', 4000))
+insert into PRODUCT (CREATION_DATE, Field8kbLong) values ('2021-01-02', replicate('B', 8000))
+insert into PRODUCT (CREATION_DATE, Field8kbLong) values ('2021-01-03', replicate('C', 4000))
 
-select substring(Field8kbLong, 1, 1) as RECORD from PRODUCT
-select substring(Field8kbLong, 1, 1) as RECORD from PRODUCT order by CREATION_DATE
+select substring(Field8kbLong, 1, 1) as RECORD, CREATION_DATE from PRODUCT
+select substring(Field8kbLong, 1, 1) as RECORD, CREATION_DATE from PRODUCT order by CREATION_DATE
