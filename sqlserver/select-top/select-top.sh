61a489c1e2e5e817be1c7bbecd74ac2c7868d0b6
ericminio <eric.mignot@gmail.com>
2021-02-20 10:48:12 -0800

    function

--- sqlserver/select-top/select-top.sh
+++ sqlserver/select-top/select-top.sh
@@ -4,8 +4,8 @@ function test_select_top_can_help_with_custom_paging {
     DIR=$(current_dir ${BASH_SOURCE[0]})
     
     executeFile ${DIR}/select-top.sql > ${DIR}/run.output
-    actual=`cat ${DIR}/run.output | sed -n '3p;4p;7p;8p' | xargs`
+    actual=`cat ${DIR}/run.output | sed -n '3p' | xargs`
 
-    assertequals "$actual" "2 3 4 5"
+    assertequals "$actual" "5"
 }
 
--- sqlserver/select-top/select-top.sql
+++ sqlserver/select-top/select-top.sql
@@ -15,10 +15,27 @@ insert into PAYMENT_BATCH (SESSION, PAYMENT_ORDER_ID, STATUS) values (2, 3, "REA
 insert into PAYMENT_BATCH (SESSION, PAYMENT_ORDER_ID, STATUS) values (2, 4, "READY")
 insert into PAYMENT_BATCH (SESSION, PAYMENT_ORDER_ID, STATUS) values (2, 5, "READY")
 GO
+CREATE OR ALTER FUNCTION payments()
+RETURNS TABLE
+AS 
+RETURN (
+    select top 2 PAYMENT_ORDER_ID 
+    from PAYMENT_BATCH 
+    where status = "READY"
+)
+GO
 
-select top 2 PAYMENT_ORDER_ID from PAYMENT_BATCH where status = "READY"
+declare @ids TABLE (ID int)
 
-update PAYMENT_BATCH set STATUS = "SUCCESS" where PAYMENT_ORDER_ID = 2
-update PAYMENT_BATCH set STATUS = "SUCCESS" where PAYMENT_ORDER_ID = 3
+insert into @ids (id) select * from payments()
+while (select count(ID) from @ids ) > 0
+begin
+    update PAYMENT_BATCH 
+        set STATUS = "SUCCESS" 
+        where PAYMENT_ORDER_ID in (select ID from @ids)
+    
+    delete from @ids
+    insert into @ids (id) select * from payments()
+end    
 
-select top 2 PAYMENT_ORDER_ID from PAYMENT_BATCH where status = "READY"
+select count(*) as COUNT from PAYMENT_BATCH where STATUS = "SUCCESS"
