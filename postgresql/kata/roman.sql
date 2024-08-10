bed3b680c96746c3ee21fe7e6eb3050923f8728d
Eric Mignot <eric.mignot@gmail.com>
2024-08-10 00:33:58 -0700

    serial dico

--- postgresql/kata/roman.sql
+++ postgresql/kata/roman.sql
@@ -112,6 +112,59 @@ begin
 end;
 $$  LANGUAGE plpgsql;
 
+insert into expectations(incoming, expected) values('CXXIX', 129);
+delete from dico;
+insert into dico(translation_priority, roman_value, decimal_value) values
+(1, 'C', 100),
+(2, 'X', 10),
+(3, 'IV', 4),
+(4, 'IX', 9),
+(5, 'I', 1)
+;
+
+insert into expectations(incoming, expected) values('MCDXCII', 1492);
+drop table if exists dico;
+create table dico(
+    translation_priority serial primary key,
+    roman_value varchar,
+    decimal_value integer
+);
+
+insert into dico(roman_value, decimal_value) values
+('M', 1000),
+('CD', 400),
+('XC', 90),
+('C', 100),
+('X', 10),
+('IV', 4),
+('IX', 9),
+('I', 1)
+;
+create or replace function roman(incoming varchar) 
+returns integer as $$
+declare
+    decimal_value integer;
+    candidate varchar;
+    candidate_value integer;
+    remaining varchar;
+    cdico cursor for select dico.roman_value, dico.decimal_value from dico order by translation_priority;
+begin
+    decimal_value := 0;
+    remaining := incoming;
+
+    for dico_entry in cdico loop
+        candidate := dico_entry.roman_value;
+        candidate_value := dico_entry.decimal_value;
+        while candidate = substring(remaining from 1 for length(candidate)) loop 
+            decimal_value := decimal_value + candidate_value;
+            remaining = substring(remaining from 1 + length(candidate));
+        end loop;
+    end loop;
+
+    return decimal_value;
+end;
+$$  LANGUAGE plpgsql;
+
 update expectations set actual = roman(incoming);
 update expectations set result = (select case when expected = actual then 'OK' else 'FAILED' end as result);
 select incoming, expected, actual, result from expectations order by id;
