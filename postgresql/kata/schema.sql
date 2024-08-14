a64fc72980829912890c606c46222371be07b595
Eric Mignot <eric.mignot@gmail.com>
2024-08-13 21:47:26 -0700

    leaner

--- postgresql/kata/schema.sql
+++ postgresql/kata/schema.sql
@@ -1,4 +1,3 @@
-drop table if exists tests;
 drop table if exists actuals;
 drop table if exists expectations;
 
@@ -12,10 +11,4 @@ create table actuals(
     expectation_id integer,
     actual integer,
     constraint fk_actuals_expectations foreign key(expectation_id) references expectations(id)
-);
-
-create table tests(
-    expectation_id integer,
-    test varchar,
-    constraint fk_tests_expectations foreign key(expectation_id) references expectations(id)
 );
