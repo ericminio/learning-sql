19e09ecd6a42f2f1767a99b6ff855f6cda7a985e
Eric Mignot <eric.mignot@gmail.com>
2024-08-13 23:06:08 -0700

    leaner

--- postgresql/configured_select/select.sh
+++ postgresql/configured_select/select.sh
@@ -3,21 +3,22 @@
 source ./postgresql/init.sh
 source ./support/utils.sh
 
-function test_works_with_hardcoded_product_id {
-    DIR=$(current_dir ${BASH_SOURCE[0]})
+DIR=$(current_dir ${BASH_SOURCE[0]})
+
+function test_works_with_hardcoded_product_id {    
     executeFile $DIR/data.sql
     executeFile $DIR/select-with-magic-number.sql > $DIR/run.output    
-    local last=`cat $DIR/run.output | head -n 3 | tail -n 1 | trim`
-
-    assertequals "$last" "2024-08-03"
+    
+    assertequals "$(last)" "2024-08-03"
 }
 
 function test_works_with_variable {
-    DIR=$(current_dir ${BASH_SOURCE[0]})
     executeFile $DIR/data.sql
-    local sql=$(sed "s/{{ product_id }}/1/g" $DIR/select-with-variable.sql)
-    execute "$sql" > $DIR/run.output    
-    local last=`cat $DIR/run.output | head -n 3 | tail -n 1 | trim`
+    execute "$(sed "s/{{ product_id }}/1/g" $DIR/select-with-variable.sql)" > $DIR/run.output    
+    
+    assertequals "$(last)" "2024-08-03"
+}
 
-    assertequals "$last" "2024-08-03"
+function last {
+    cat $DIR/run.output | head -n 3 | tail -n 1 | trim
 }
