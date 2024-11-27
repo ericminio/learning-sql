f9dc75a7c507ec6a9958d76ec240a91248f1a9d1
Eric Mignot <eric.mignot@gmail.com>
2024-11-26 22:58:47 -0800

    xbulk can load 2 fields

--- mysql/load/xbulk_tests.sh
+++ mysql/load/xbulk_tests.sh
@@ -12,3 +12,16 @@ function test_xbulk_can_load_one_field {
 
     assertequals "$value" "alice bob"
 }
+
+function test_xbulk_can_load_two_fields {
+    DIR=$(current_dir ${BASH_SOURCE[0]})
+    xbulk "./mysql/load/xbulktwo.csv"
+
+    execute "
+        select *
+        from xbulktwo
+    " > $DIR/run.output
+    value=`cat ${DIR}/run.output | extract_query_result`
+
+    assertequals "$value" "alice 25 bob 21"
+}
