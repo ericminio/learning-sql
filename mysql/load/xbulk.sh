1a8033cb14049e2984aadfc5317a90b6b7c1a975
Eric Mignot <eric.mignot@gmail.com>
2024-11-26 23:19:13 -0800

    varchar 100

--- mysql/load/xbulk.sh
+++ mysql/load/xbulk.sh
@@ -2,11 +2,10 @@
 
 function xbulk {
     local FILE=$1
-    local TABLE=`echo $FILE | grep -o "[^/]*$" | cut -d '.' -f1`
-    local FIELDS=`head -1 $FILE | sed 's/,/ varchar(10),/' | sed 's/$/ varchar(10)/'`
+    local TABLE=`echo $FILE | grep -o "[^/]*$" | cut -d'.' -f1`
+    local FIELDS=`head -1 $FILE | sed 's/,/ varchar(100),/' | sed 's/$/ varchar(100)/'`
     execute "
         drop table if exists $TABLE;
         create table $TABLE($FIELDS);"
-        
     bulk_copy $FILE $TABLE
 }
