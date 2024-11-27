7697a57fb549124d2d3dc6740df511366ea39518
Eric Mignot <eric.mignot@gmail.com>
2024-11-26 22:34:44 -0800

    bulk truncates too long fields

--- mysql/load/bulk_tests.sh
+++ mysql/load/bulk_tests.sh
@@ -41,3 +41,42 @@ function test_load_data_from_csv_resisting_comma_in_data {
 
     assertequals "$value" "AA, BB 2"
 }
+
+function test_bulk_can_load_boolean {
+    DIR=$(current_dir ${BASH_SOURCE[0]})
+    execute "
+        drop table if exists events;
+        create table events(
+            id varchar(10), 
+            external_id varchar(10),
+            flag boolean
+        );"
+    bulk_copy "./mysql/load/boolean.csv" "events" 
+
+    execute "
+        select count(id)
+        from events
+        where flag = 1" > $DIR/run.output
+    value=`cat ${DIR}/run.output | extract_query_result`
+
+    assertequals "$value" "5"
+}
+
+function test_bulk_truncates_too_long_fields {
+    DIR=$(current_dir ${BASH_SOURCE[0]})
+    execute "
+        drop table if exists events;
+        create table events(
+            one varchar(10), 
+            two varchar(5),
+            three varchar(10)
+        );"
+    bulk_copy "./mysql/load/toolong.csv" "events" 
+
+    execute "
+        select *
+        from events" > $DIR/run.output
+    value=`cat ${DIR}/run.output | extract_query_result`
+
+    assertequals "$value" "before 12345 after"
+}
