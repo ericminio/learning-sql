bf038e86cf03417287c1615e257ed5c3f48b5386
Eric Mignot <eric.mignot@gmail.com>
2024-11-26 23:05:47 -0800

    reduce noise

--- mysql/load/xbulk.sh
+++ mysql/load/xbulk.sh
@@ -4,11 +4,9 @@ function xbulk {
     local FILE=$1
     local TABLE=`echo $FILE | grep -o "[^/]*$" | cut -d '.' -f1`
     local FIELDS=`head -1 $FILE | sed 's/,/ varchar(10),/' | sed 's/$/ varchar(10)/'`
-
     execute "
         drop table if exists $TABLE;
-        create table $TABLE(
-            $FIELDS
-        );"
+        create table $TABLE($FIELDS);"
+        
     bulk_copy $FILE $TABLE
 }
--- mysql/load/xbulk_tests.sh
+++ mysql/load/xbulk_tests.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
 function test_xbulk_can_load_one_field {
-    DIR=$(current_dir ${BASH_SOURCE[0]})
     xbulk "./mysql/load/xbulkone.csv"
-
-    execute "
-        select *
-        from xbulkone
-    " > $DIR/run.output
-    value=`cat ${DIR}/run.output | extract_query_result`
+    value=`execute "select name from xbulkone" | extract_query_result`
 
     assertequals "$value" "alice bob"
 }
 
 function test_xbulk_can_load_two_fields {
-    DIR=$(current_dir ${BASH_SOURCE[0]})
     xbulk "./mysql/load/xbulktwo.csv"
-
-    execute "
-        select *
-        from xbulktwo
-    " > $DIR/run.output
-    value=`cat ${DIR}/run.output | extract_query_result`
+    value=`execute "select name, day from xbulktwo" | extract_query_result`
 
     assertequals "$value" "alice 25 bob 21"
 }
